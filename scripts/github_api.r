87f7bb15bb9926963b245447f0c2bcb3483a99ea
Jeremy Stanley <jstanley@sailthru.com>
2014-09-14 11:12:44 -0400

    script to pull data from github API

---
+++ scripts/github_api.r
@@ -0,0 +1,51 @@
+# Script to get github commit json data
+#
+# Notes:
+#  1) this script must be run interactively (e.g., source("file.r"))
+#  2) you must set JPLYR_* environment variables used below
+
+require(httr)
+
+#' Gets github data from API
+#'
+#' @param url the github API url to GET
+#' @param token the oauth2.0 token
+get_github_data <- function(url, token) {
+  
+  # Get request
+  request <- with_config(config(token = token), GET(url))
+
+  # Convert to JSON
+  readBin(request$content, character())
+
+}
+
+# Setup app
+github_app <- oauth_app("jplyr_demo",
+  key = Sys.getenv("JPLYR_GITHUB_KEY"),
+  secret = Sys.getenv("JPLYR_GITHUB_SECRET")
+)
+
+# Get endpoing
+github_endpoint <- oauth_endpoints("github")
+
+# Get credentials
+github_token <- oauth2.0_token(github_endpoint, github_app)
+
+# Get commits in a repo
+dplyr_commits <- get_github_data(
+  "https://api.github.com/repos/hadley/dplyr/commits",
+  github_token)
+save(dplyr_commits, file = "../data/dplyr_commits.rda")
+
+# Get dplyr issues
+dplyr_issues <- get_github_data(
+  "https://api.github.com/repos/hadley/dplyr/issues",
+  github_token)
+save(dplyr_commits, file = "../data/dplyr_issues.rda")
+
+  
+
+
+
+
