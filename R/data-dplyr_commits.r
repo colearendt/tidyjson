3bd5f2074e7309821535f66ade3d7b2fd9902924
Jeremy Stanley <jstanley@sailthru.com>
2014-09-14 11:06:20 -0400

    add data from dplyr repo via github API

---
+++ R/data-dplyr_commits.r
@@ -0,0 +1,8 @@
+#' Commit data for the dplyr repo from github API
+#' 
+#' @docType data
+#' @name dplyr_commits
+#' @usage dplyr_commits
+#' @format JSON
+#' @examples
+#' writeLines(dplyr_commits)
---
+++ R/data-dplyr_issues.r
@@ -0,0 +1,8 @@
+#' Issue data for the dplyr repo from github API
+#' 
+#' @docType data
+#' @name dplyr_issues
+#' @usage dplyr_issues
+#' @format JSON
+#' @examples
+#' writeLines(dplyr_issues)
