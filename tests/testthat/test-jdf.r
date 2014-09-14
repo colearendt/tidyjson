9d19c79a356ce8d7f2ddaa48027312476748f5ae
Jeremy Stanley <jstanley@sailthru.com>
2014-09-14 11:21:23 -0400

    add first tests

---
+++ tests/testthat/test-jdf.r
@@ -0,0 +1,40 @@
+context("jdf: as.jdf.character")
+
+test_that("correctly parses length(json) == 1", {
+  expect_identical(
+    as.jdf('{"name": "bob", "age": 32}'),
+    structure(
+      data.frame(document.id = 1L),
+      JSON = list(list(name = "bob", age = 32)),
+      class = c("jdf", "data.frame")
+    )
+  )
+})
+
+test_that("correctly parses length(json) > 1", {
+  expect_identical(
+    as.jdf(
+      c('{"name": "bob", "age": 32}',
+        '{"name": "susan", "age": 25}')
+    ),
+    structure(
+      data.frame(document.id = 1L:2L),
+      JSON = list(
+        list(name = "bob", age = 32),
+        list(name = "susan", age = 25)
+      ),
+      class = c("jdf", "data.frame")
+    )
+  )
+})
+
+test_that("currectly parses length(json) == 0", {
+  expect_identical(
+    as.jdf(character(0)),
+    structure(
+      data.frame(document.id = integer(0)),
+      JSON = list(),
+      class = c("jdf", "data.frame")
+    )
+  )
+})
