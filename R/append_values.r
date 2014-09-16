b929b6d355e4b711a22372562f804c4479bb7788
Jeremy Stanley <jstanley@sailthru.com>
2014-09-16 09:44:01 -0400

    #6 refactor append_values to use a factory and create _number and _logical versions

--- R/append_values.r
+++ R/append_values.r
@@ -3,42 +3,45 @@
 #' @name append_values
 NULL
 
-#' @export
-#' @rdname append_values
-append_values_string <- function(x, column.name = "string") {
+#' Creates the append_values_* functions
+append_values_factory <- function(type, na_value) {
   
-  assert_that(is.tbl_json(x))
+  function(x, column.name = type) {
+    
+    assert_that(is.tbl_json(x))
   
-  # Extract json 
-  json <- attr(x, "JSON")
-  
-  # Determine types
-  types <- determine_types(json)
-  
-  # Initialize column to NAs
-  column <- rep(NA_character_, nrow(x))
-  
-  # Identify correct types
-  correct <- types == "string"
-  
-  # Set correct to data
-  column[correct] <- unlist(json[correct])
-  
-  # Add as a column to x
-  x[column.name] <- column
-  
-  tbl_json(x, json)
-  
+    # Extract json 
+    json <- attr(x, "JSON")
+    
+    # Determine types
+    types <- determine_types(json)
+    
+    # Initialize column to NAs
+    column <- rep(na_value, nrow(x))
+    
+    # Identify correct types
+    correct <- types == type
+    
+    # Set correct to data
+    column[correct] <- unlist(json[correct])
+    
+    # Add as a column to x
+    x[column.name] <- column
+    
+    tbl_json(x, json)
+    
+  }
 }
+  
 
 #' @export
 #' @rdname append_values
-append_values_number <- function(x, column.name = "number") {
-  
-}
+append_values_string <- append_values_factory("string", NA_character_)
 
 #' @export
 #' @rdname append_values
-append_values_logical <- function(x, column.name = "logical") {
-  
-}
+append_values_number <- append_values_factory("number", NA_real_)
+
+#' @export
+#' @rdname append_values
+append_values_logical <- append_values_factory("logical", NA)
--- tests/testthat/test-append_values.r
+++ tests/testthat/test-append_values.r
@@ -1,6 +1,6 @@
-context("append_values_string")
+context("append_values")
 
-test_that("works with simple input", {
+test_that("has correct complete structure with simple input", {
     
     json <- '[{"name": "anne"}, {"name": "bob"}, {"name": "george"}]'
 
@@ -22,7 +22,7 @@ test_that("works with simple input", {
   }
 )
 
-test_that("works with value array", {
+test_that("string works with value array", {
     
     json <- '["a", "b"]'
 
@@ -42,7 +42,7 @@ test_that("works with value array", {
   }
 )
 
-test_that("handles nulls", {
+test_that("string works with simple input", {
     
     json <- '["a", "b", null]'
 
@@ -53,3 +53,49 @@ test_that("handles nulls", {
     
   }
 )
+
+test_that("number works with simple input", {
+    
+    json <- '[1, 2, null]'
+
+    expect_identical(
+      (json %>% as.tbl_json %>% gather_array %>% append_values_number)$number,
+      c(1, 2, NA_real_)
+    )
+    
+  }
+)
+
+test_that("logical works with simple input", {
+    
+    json <- '[true, false, null]'
+
+    expect_identical(
+      (json %>% as.tbl_json %>% gather_array %>% append_values_logical)$logical,
+      c(TRUE, FALSE, NA)
+    )
+    
+  }
+)
+
+test_that("handles mixed input as appropriate NA", {
+    
+    data <- '["a", 1, true, null]' %>% as.tbl_json %>% gather_array 
+
+    expect_identical(
+      (data %>% append_values_string)$string,
+      c("a", rep(NA_character_, 3))
+    )
+    expect_identical(
+      (data %>% append_values_number)$number,
+      c(NA_real_, 1, rep(NA_real_, 2))
+    )
+    expect_identical(
+      (data %>% append_values_logical)$logical,
+      c(rep(NA, 2), TRUE, NA)
+    )
+    
+  }
+)
+
+  
