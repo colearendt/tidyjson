be072e65ee5c19436d643009715e10d796c3944f
Jeremy Stanley <jstanley@sailthru.com>
2014-09-21 12:57:16 -0400

    #14 add json_lenghts

---
+++ R/json_lengths.r
@@ -0,0 +1,22 @@
+#' Add a column that tells the 'length' of the data in the root of the JSON
+#' 
+#' @param x a tbl_json object
+#' @param column.name the name to specify for the length column
+#' @return a tbl_json object with column.name column that tells the length
+#' @export
+json_lengths <- function(x, column.name = "length") {
+  
+  assert_that(is.tbl_json(x))
+  
+  # Extract json 
+  json <- attr(x, "JSON")
+  
+  # Determine lengths
+  lengths <- vapply(json, length, integer(1))
+  
+  # Add as a column to x
+  x[column.name] <- lengths
+  
+  tbl_json(x, json)
+  
+}
---
+++ tests/testthat/test-json_lengths.r
@@ -0,0 +1,51 @@
+context("json_lengths")
+
+test_that("works for arrays", {
+    
+    json <- c('[]', '[1]', '[1, 2]')
+    expect_identical(
+      json %>% as.tbl_json %>% json_lengths %>% `$`(length),
+      c(0L, 1L, 2L)
+    )
+    
+  }
+)
+
+test_that("works for objects", {
+    
+    json <- c('{}', '{"k":"v"}', '{"k1":"v1", "k2":"v2"}') 
+    expect_identical(
+      json %>% as.tbl_json %>% json_lengths %>% `$`(length),
+      c(0L, 1L, 2L)
+    )
+    
+  }
+)
+
+test_that("works for scalars", {
+    
+    json <- c('[1, "a", true]') 
+    expect_identical(
+      json %>% as.tbl_json %>% gather_array %>% json_lengths %>% `$`(length),
+      rep(1L, 3)
+    )
+     
+  }
+)
+
+test_that("works for emtpy objects", {
+    
+    json <- character(0) 
+    expect_identical(
+      json %>% as.tbl_json %>% json_lengths %>% `$`(length),
+      integer(0)
+    )
+    
+    json <- c('[null, [], {}]') 
+    expect_identical(
+      json %>% as.tbl_json %>% gather_array %>% json_lengths %>% `$`(length),
+      rep(0L, 3)
+    )
+     
+  }
+)
