9e6a45226aa214eb774e10a84f3d35fcfd5875b8
Jeremy <jeremy.stanley@instacart.com>
2016-08-21 13:09:20 -0700

    use purrr in path #1

--- R/path.r
+++ R/path.r
@@ -1,19 +1,19 @@
 #' Prepare a path from ...
 #' @param ... character strings to construct into a path
 prep_path <- function(...) {
-  
+
   # Conver to a list
   path <- list(...)
-  
+
   # Check type
-  path_is_char <- vapply(path, inherits, logical(1), "character")
-  path_len_1 <- vapply(path, length, integer(1)) == 1
+  path_is_char <- map_lgl(path, inherits, "character")
+  path_len_1 <- map_int(path, length) == 1
   if (!all(path_is_char & path_len_1))
     stop("malformed path")
-  
+
   # Unlist
   path <- unlist(path)
-  
+
   path
 }
 
@@ -21,14 +21,14 @@ prep_path <- function(...) {
 #' @param l a list
 #' @param path a path of keys to follow
 list_path <- function(l, path) {
-  
+
   # Unwind this step
-  l <- lapply(l, "[[", path[1])
-  
-  # Keep going if more remains in the path 
+  l <- map(l, `[[`, path[1])
+
+  # Keep going if more remains in the path
   if (length(path) > 1)
     l <- list_path(l, path[-1])
-  
+
   l
 }
 
@@ -36,13 +36,13 @@ list_path <- function(l, path) {
 #' @param l a list
 #' @param replace what to replace NULLs in the list with
 replace_nulls <- function(l, replace) {
-  
+
   # Find the nulls
-  nulls <- vapply(l, is.null, logical(1))
-  
+  nulls <- map_lgl(l, is.null)
+
   if (any(nulls))
     l[nulls] <- replace
 
   l
-  
+
 }
--- R/tidyjson-package.r
+++ R/tidyjson-package.r
@@ -4,6 +4,6 @@
 #' @docType package
 #' @import assertthat
 #' @import jsonlite
-#' @importFrom purrr map map_lgl
+#' @importFrom purrr map map_lgl map_int
 #' @import dplyr
 NULL
