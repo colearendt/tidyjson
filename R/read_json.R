bee34964c17dc8397d0ce2b8828eb00acc525243
Jeremy <jeremy.stanley@instacart.com>
2016-09-06 10:08:29 -0700

    #42 update documentation to read_json

--- R/read_json.R
+++ R/read_json.R
@@ -1,10 +1,12 @@
-#' Reads JSON from an input uri (file, url, ...) and returns a tbl_json
+#' Reads JSON from an input uri (file, url, ...) and returns a
+#' \code{\link{tbl_json}} object
 #'
 #' @param path to some json data
 #' @param format
-#'   If "json", process the data like one large JSON record.
-#'   If "jsonl", process the data one JSON record per line (json lines format)
-#'   If "infer", the format is the suffix of the given filepath.
+#'   If \code{"json"}, process the data like one large JSON record.
+#'   If \code{"jsonl"}, process the data one JSON record per line (json lines
+#'   format).
+#'   If \code{"infer"}, the format is the suffix of the given filepath.
 #' @return a \code{\link{tbl_json}} object
 #' @export
 read_json <- function(path, format = c("json", "jsonl", "infer")) {
