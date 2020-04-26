e90e50252ddc992de5369c96a51938c4ff604289
Cole Arendt <cole.arendt@outlook.com>
2020-04-25 22:40:03 -0400

    switch test to the actual failing case

--- tests/testthat/test-tbl_json.R
+++ tests/testthat/test-tbl_json.R
@@ -561,8 +561,8 @@ test_that('dplyr::select works', {
   expect_silent(hm <- as_tbl_json(json) %>% select(document.id))
   
   # today behavior:
-  expect_error(select(f, ..JSON), "not supported today")
-  expect_error(select(f, !!!c("..JSON")), "not supported today")
+  expect_error(select(f, ..JSON), class = "vctrs_error_subscript_oob")
+  expect_error(select(f, !!!c("..JSON")), class = "vctrs_error_subscript_oob")
   
   # desired behavior:
   skip("failing: need a way to edit ... and remove ..JSON")
